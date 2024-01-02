9f29fa15b29759df13b92a1ba57d5d0e90e22328
Stewart Bustard <stewart.bustard@gmail.com>
2024-01-02 15:53:45 -0500

    User logged in forms

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user
 import sqlalchemy as sa 
 from app import db 
@@ -15,7 +15,6 @@ from urllib.parse import urlsplit
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Stewart'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -26,18 +25,11 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
+
+    
 @app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/login', models=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -53,7 +45,6 @@ def login():
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-        '''return redirect(url_for('index'))'''
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
